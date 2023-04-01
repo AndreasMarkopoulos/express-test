ab88021fd8e91eb133123a43c622b5065409bbf5
amarkopoulos <amarkopoulos@mtg-hellas.gr>
2023-04-01 22:37:27 +0300

    minor change

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.listen(port, (err, res) => {
 app.post('/generate-meal-plan', async function (req, res) {
     try {
         if(!req.body) {
-            throw new Error('Empty request body')
+            throw new Error('Empty request body'+req)
         }
         const input = req.body.input;
         const userId = req.body.userId;
